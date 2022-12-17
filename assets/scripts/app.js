09c846ec93ae138c3656cee4c530a749715deeb4
matheusOliv23 <matheushenri1996@gmail.com>
2022-12-17 12:04:00 -0300

    feat: add attack, strong attack and heal functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 18;
 const MONSTER_ATTACK_VALUE = 15;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife
@@ -7,10 +9,7 @@ let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE)
-    currentMonsterHealth -= damage
-
+function endRound() {
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
     currentPlayerHealth -= playerDamage
 
@@ -23,4 +22,39 @@ function attackHandler() {
     }
 }
 
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode === 'ATTACK') {
+        maxDamage = ATTACK_VALUE
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE
+    }
+    const damage = dealMonsterDamage(maxDamage)
+    currentMonsterHealth -= damage
+    endRound()
+}
+
+function attackHandler() {
+    attackMonster('ATTACK')
+}
+
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK')
+}
+
+function healPlayerHandler() {
+    let healValue;
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+        alert('You cant heal')
+        healValue = chosenMaxLife - currentPlayerHealth
+    } else {
+        healValue = HEAL_VALUE
+    }
+    increasePlayerHealth(healValue)
+    currentPlayerHealth += healValue
+    endRound()
+}
+
 attackBtn.addEventListener('click', attackHandler)
+strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
