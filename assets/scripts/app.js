eae74eaac54b99328dbf12c31ab68fadaa45f58a
matheusOliv23 <matheushenri1996@gmail.com>
2022-12-17 13:30:49 -0300

    refactor: creating global variables to store values

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,17 @@ const STRONG_ATTACK_VALUE = 18;
 const MONSTER_ATTACK_VALUE = 15;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const MODE_ATTACK = 'ATTACK'
+const MODE_STRONG_ATTACK = 'STRONG_ATTACK'
+
+const enteredValue = prompt('Maximum life for you and the monster', '100')
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+    chosenMaxLife = 100
+}
+
 let currentMonsterHealth = chosenMaxLife
 let currentPlayerHealth = chosenMaxLife
 let hasBonusLife = true;
@@ -48,9 +58,9 @@ function endRound() {
 
 function attackMonster(mode) {
     let maxDamage;
-    if (mode === 'ATTACK') {
+    if (mode === MODE_ATTACK) {
         maxDamage = ATTACK_VALUE
-    } else if (mode === 'STRONG_ATTACK') {
+    } else if (mode === MODE_STRONG_ATTACK) {
         maxDamage = STRONG_ATTACK_VALUE
     }
     const damage = dealMonsterDamage(maxDamage)
@@ -59,11 +69,11 @@ function attackMonster(mode) {
 }
 
 function attackHandler() {
-    attackMonster('ATTACK')
+    attackMonster(MODE_ATTACK)
 }
 
 function strongAttackHandler() {
-    attackMonster('STRONG_ATTACK')
+    attackMonster(MODE_STRONG_ATTACK)
 }
 
 function healPlayerHandler() {
